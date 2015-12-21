7dcc01e3c1ce988ebbfe56fcf38753be4f8124d1
xelita <admin@xelita.com>
2015-12-21 23:25:20 +0100

    webpack support

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  entry: "./js/main.js",
+  output: {
+    path: __dirname + "/dist",
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: { // 'babel-loader' is also a legal name to reference
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  }
+};
