a6f490497928eddafb05659b382270564c384fb0
xelita <admin@xelita.com>
2015-12-22 00:03:29 +0100

    nested components

--- js/main.js
+++ js/main.js
@@ -2,13 +2,38 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+// HelloMessage component
+
 var HelloMessage = React.createClass({
-  render: function() {
-    return <p>Hello {this.props.name}!</p>;
-  }
+    render: function() {
+        return <li>Hello {this.props.name}!</li>;
+    }
 });
 
+// Messages list component
+
+var MessageList = React.createClass({
+    render: function() {
+        var messagesList = this.props.messages || [];
+        var componentsList = [];
+        messagesList.forEach(function(item){
+            componentsList.push(<HelloMessage key={item.name} name={item.name}/>);
+        });
+
+        return <ul>{componentsList}</ul>;
+    }
+});
+
+// Application rendering
+
+var messages = [
+    {name: "xelita"},
+    {name: "prism"},
+    {name: "cybersami"},
+    {name: "mrtot"},
+];
+
 ReactDOM.render(
-  <HelloMessage name="Xelita" />,
-  document.getElementById("container")
+    <MessageList messages={messages}/>,
+    document.getElementById("container")
 );
